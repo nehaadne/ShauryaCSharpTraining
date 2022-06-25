04c5e89a4a7496337582d6bcd878cc8179e6a9e0
u <u@DESKTOP-A9P3QFO>
2022-06-25 14:28:13 +0530

    Add project files.

---
+++ Collection/List.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShauryaCSharpTraining.Collection
+{
+    class List
+
+    {
+        static void Main()
+        {
+            List<string> myList = new List<string>() {
+         "neha",
+         "swati",
+         "amit",
+         "mansi"
+        };
+
+            Console.Write("Initial list...");
+            foreach (string list in myList)
+            {
+                Console.WriteLine(list);
+            }
+
+            Console.Write("Using Remove() method...");
+            myList.Remove("swati");
+
+            foreach (string list in myList)
+            {
+                Console.WriteLine(list);
+            }
+
+            Console.Write("Using RemoveAt() method...");
+            myList.RemoveAt(1);
+
+            foreach (string list in myList)
+            {
+                Console.WriteLine(list);
+            }
+        }
+
+    }
+
+}    
+
+
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+//class A
+//{
+//    public static void Main()
+//    {
+//        try
+//        {
+//            Console.WriteLine("Enter number");
+//            int n = int.Parse(Console.ReadLine());
+//            Console.WriteLine("try ends");
+//        }
+//        catch(FormatException e)
+//        {
+//            Console.WriteLine(e.Message);
+//            Console.WriteLine("wrong input");
+//        }
+//        for(int i=2;i<=20;i=i+2)
+//            Console.WriteLine(i);
+//    }
+//}
+
+class InvalidPasswordException : Exception
+{
+    public InvalidPasswordException(string msg) : base(msg)
+    {
+
+    }
+    public InvalidPasswordException()
+    {
+
+    }
+    class Demo
+    {
+        string pass;
+
+        public string Password
+        {
+            set
+            {
+                if (value.Length < 5)
+                    throw new InvalidPasswordException();
+                pass = value;
+            }
+        }
+        class Empp
+        {
+            static void Main1(string[] args)
+            {
+                Empp e1 = new Empp();
+                Demo ob = new Demo();
+                try
+                {
+                    ob.Password = "neha";
+                }
+                catch (InvalidPasswordException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
